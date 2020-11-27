3ee476488fcdf95c5e03c1cfb81c59f383186341
thedarklord <vader@deathstar.empire>
2020-11-27 18:49:45 +0100

    fix side effects

--- risk_aversion.py
+++ risk_aversion.py
@@ -3,37 +3,29 @@
 """
 
 
-def power_utility_loss_aversion(z_list, gamma, lambda_):
-    """Return the utility evaluation of a vector of monetary
-    quantities *z* under power utility with utility curvature
-    parameter *gamma* and loss aversion parameter *lambda_*.
+def power_utility_loss_aversion(z, ɣ, λ):
+    """Return the utility evaluation of a monetary quantity z under power
+    utility with utility curvature parameter ɣ and loss aversion
+    parameter λ.
 
     """
 
-    out = z_list
-    for i, z in enumerate(z_list):
-        if z >= 0:
-            out[i] = z ** (1 - gamma)
-        else:
-            out[i] = -lambda_ * (-z) ** (1 - gamma)
+    out = z ** (1 - ɣ) if z >= 0 else -λ * (-z) ** (1 - ɣ)
 
     return out
 
 
 # Define the characteristics of the individual.
-power_coefficient = 0.5
-loss_aversion_coefficient = 2.0
+power_coefficients = [0.5, 0.5]
+loss_aversion_coefficients = [2.0, 2.0]
 
 # Define the monetary payoffs.
 payoffs = [500, -550]
 
-# Compute the utility values
-evaluations = power_utility_loss_aversion(
-    payoffs, power_coefficient, loss_aversion_coefficient
+# Compute the utility values and print them to the screen.
+out = map(
+    power_utility_loss_aversion, payoffs, power_coefficients, loss_aversion_coefficients
 )
 
-# Print the utility values to the screen.
-print(evaluations)
-
-# Goal: Understand what happened here.
-print(payoffs)
+for item in out:
+    print(item)
